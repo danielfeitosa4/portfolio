3df28b5e7e4bb8fb49b18e9d1295694084d18f37
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2022-01-24 08:00:47 -0300

    adicionando cometário

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
+  # Ativando modo de desenvolvedor, agora toda modificação que eu fizer no meu template será atualizada automaticamente. 
     app.run(debug=True)
