805a5f6b3b273c49b90aeab572ddee05865e1963
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2022-02-15 15:51:56 -0300

    mais animacoes

--- static/script.js
+++ static/script.js
@@ -29,8 +29,19 @@ navItem.forEach(item => {
 const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
-  const windowTop = window.pageYOffset
-  console.log(windowTop)
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+  
+  item.forEach(element => {
+    if (windowTop > element.offsetTop){
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  })
 }
 
 animeScroll()
+
+window.addEventListener("scroll", ()=>{
+  animeScroll();
+})
